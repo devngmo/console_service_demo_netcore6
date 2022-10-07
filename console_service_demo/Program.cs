db806cc27faa130a2caf45fdb26f91c6f2f79fc2
Tran Manh Linh <devngmo@gmail.com>
2022-10-07 19:09:38 +0700

    update

---
+++ console_service_demo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int counter = 1;
+int nReset = 0;
+while (true)
+{
+    Console.WriteLine($"Reset [{nReset}] counter {counter}");
+    Thread.Sleep(3000);
+    counter++;
+    if (counter > 999999999)
+    {
+        counter = 1;
+        nReset++;
+    }
+}
